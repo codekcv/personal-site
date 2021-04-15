110da03734a736d76bc1c33c8c954efc76fa1cf1
Christian Villamin <ChristianVillamin31@gmail.com>
2021-04-16 01:03:10 +0800

    update config string

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Christian Villamin's Site`,
+    description: `My personal slice of the internet.`,
+    author: `@codekcv`,
   },
   plugins: [
     `gatsby`,
