e02c8c670e450cf9e4ad01631156dd8d52a5c282
Christian Villamin <ChristianVillamin31@gmail.com>
2021-04-16 01:04:12 +0800

    fix linario alias

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,13 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const path = require("path")
 
-// You can delete this file if you're not using it
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        "@reach/router": path.dirname(
+          require.resolve(`@gatsbyjs/reach-router/package.json`)
+        ),
+      },
+    },
+  })
+}
