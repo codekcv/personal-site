63f6615865e404100e27735f8b764a1e4ef3de03
Christian Villamin <ChristianVillamin31@gmail.com>
2021-04-16 18:37:21 +0800

    index update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,22 @@
 import * as React from "react"
-
+import Card from "src/components/Card"
 import Layout from "src/components/Layout"
 import SEO from "src/components/SEO"
 import About from "src/sections/About"
 import Introduction from "src/sections/Introduction"
 
-const IndexPage: React.FC = () => (
-  <Layout>
-    <SEO />
-    <Introduction />
-    <About />
-  </Layout>
-)
+const sections = [Introduction, About, About, About, About, About]
+
+const IndexPage: React.FC = () => {
+  return (
+    <Layout>
+      <SEO />
+
+      {sections.map(Section => (
+        <Card Component={<Section />} />
+      ))}
+    </Layout>
+  )
+}
 
 export default IndexPage
