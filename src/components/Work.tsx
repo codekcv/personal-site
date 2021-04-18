832ce9c1f8a8443a30a74e52c432f46b886586f7
Christian Villamin <ChristianVillamin31@gmail.com>
2021-04-18 22:09:03 +0800

    work section update

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,21 +1,174 @@
 import * as React from "react"
 import styled from "styled-components"
 
-const Container = styled.div``
+const Container = styled.div`
+  > .date {
+    margin-top: -16px;
+    font-size: 0.8rem;
+    font-style: italic;
+  }
+
+  .divider {
+    border-bottom: 1px solid rgba(50, 100, 100, 0.15);
+    margin-bottom: 18px;
+  }
+
+  > .hov-project {
+    margin-bottom: 3rem;
+
+    .sub {
+      margin-top: -16px;
+      font-size: 0.8rem;
+      font-style: italic;
+    }
+  }
+
+  .article {
+    display: block;
+    margin-bottom: 18px;
+  }
+
+  @media (max-width: 800px) {
+    > .date {
+      margin-top: -14px;
+    }
+  }
+`
+
+const Wallet = () => (
+  <div className="hov-project">
+    <h3>Enterprise Dashboard Project 1</h3>
+    <p className="sub">Project I mainted and developed in HOV.</p>
+    <p>
+      Worked with a team of designers, QA Specialists, backend engineers, and
+      product managers on an information management application for real-time
+      game and player data processing. Worked as a core member of the front-end
+      team in an agile environment that maintain, develop, test, and ships
+      features.
+    </p>
+    <h4>Notable Technologies Used</h4>
+    <ul className="tech-used">
+      <li>React</li>
+      <li>Redux</li>
+      <li>TypeScript</li>
+      <li>Apollo GraphQL</li>
+      <li>Ant Design</li>
+      <li>styled-components</li>
+      <li>Recharts</li>
+      <li>React Hook Form</li>
+      <li>React Testing Library</li>
+      <li>Jest</li>
+      <li>CircleCI</li>
+    </ul>
+  </div>
+)
+
+const Her = () => (
+  <>
+    <div className="hov-project">
+      <h3>
+        HER (
+        <a href="https://www.her.vn/" target="_blank" rel="noopener noreferrer">
+          her.vn
+        </a>
+        )
+      </h3>
+      <div className="sub">Project I built for HOV</div>
+
+      <p>
+        HER is a blogging site about better living of Vietnamese women. A
+        project I built from scratch and as a sole front-end developer.
+      </p>
+
+      <ul className="tech-used">
+        <li>NextJS</li>
+        <li>Strapi (CMS)</li>
+        <li>react markdown</li>
+        <li>Zustand</li>
+        <li>SWR</li>
+        <li>graphql-request</li>
+        <li>styled-components</li>
+        <li>ConvertKit</li>
+        <li>Vercel deployment</li>
+        <li>react</li>
+      </ul>
+    </div>
+  </>
+)
+
+const HOV = () => (
+  <div className="hov-project">
+    <h3>
+      HOV Website (
+      <a href="https://hov.co/" target="_blank" rel="noopener noreferrer">
+        hov.co
+      </a>
+      )
+    </h3>
+    <div className="sub">Project I collaborated in HOV</div>
+
+    <p>
+      The revamped website of High Output Ventures built from scratch from
+      collaboration with the FE team. My main part of this is creating the
+      main/index page and its animations.
+    </p>
+
+    <ul className="tech-used">
+      <li>NextJS</li>
+      <li>TypeScript</li>
+      <li>tailwindCSS</li>
+      <li>Framer Motion</li>
+      <li>react-intersection-obvserver</li>
+      <li>React Hook Form</li>
+      <li>Apollo GraphQL</li>
+    </ul>
+  </div>
+)
 
 const Work: React.FC = () => (
   <Container id="work">
     <h1>* WORK EXPERIENCE</h1>
 
     <h2>
-      High Output Ventures (<a href="https://hov.co/">hov.co</a>)
+      High Output Ventures (
+      <a href="https://hov.co/" target="_blank" rel="noopener noreferrer">
+        hov.co
+      </a>
+      )
     </h2>
+    <p className="date">Dec 2019 - Current | Front-End Developer</p>
 
-    <p>Dec 2019 - Current | Front-End Developer</p>
-    <p>Project I maintained and developed in HOV.</p>
-    <p>Project I collaborated in HOV.</p>
-    <p>Project I built for HOV.</p>
-    <p>I also wrote an article in our HOV site.</p>
+    <Wallet />
+    <Her />
+    <HOV />
+
+    <p>
+      I also wrote an article in our HOV site. You can read it here at your own
+      peril.
+    </p>
+    <a
+      className="article"
+      href="https://www.hov.co/blog/post/ill-tell-you-6-specific-things-to-stop-doing-and-win-at-life-could-be-offensive"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      How to win at live. Could be offensive.
+    </a>
+
+    <div className="divider" />
+
+    <h2>{`{ Insert Company Name }`}</h2>
+    <div className="date">202X - 20XX | Software Engineer</div>
+    <p>Hire me. Making everything win.</p>
+
+    <div className="divider" />
+
+    <h2>Sheperd</h2>
+    <div className="date">2060 - To Death</div>
+    <p className="work-description">
+      Milking cows and tending sheeps in a peaceful farm at a mountain side with
+      a waterfall and a river crossing.
+    </p>
   </Container>
 )
 
