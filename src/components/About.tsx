38cb6c3520e0d0d28c79fbbf71f6db16a6442c36
Christian Villamin <ChristianVillamin31@gmail.com>
2021-04-18 22:08:26 +0800

    aboiut update

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -14,10 +14,11 @@ const About: React.FC = () => (
     <h1>* ABOUT ME AND STUFF</h1>
 
     <p>
-      i'm a software engineer. tinker lord. coding god. immortal. loves winning.
-      i don't play games but i play sometimes. dank memes are fun. piano. cats.
-      a little design. nature is life. no interest in politics and religion, i
-      respect your view. detective conan fanboi. happy kid :)
+      i'm a software engineer specializing on the front-end. loves winning.
+      coding god and tinker lord. i don't play games but i play sometimes.
+      memes. piano. cats. coffee. immortality. no interest in politics and
+      religion, i respect your view. nature is life. detective conan fanboi.
+      happy kid :)
     </p>
   </Container>
 )
