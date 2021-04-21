95dd38b895bd12ac2e92cdbc8c9439ce00650125
Christian Villamin <ChristianVillamin31@gmail.com>
2021-04-22 05:15:27 +0800

    blog update

--- src/components/Blogs.tsx
+++ src/components/Blogs.tsx
@@ -6,7 +6,15 @@ const Container = styled.div``
 const Blogs: React.FC = () => (
   <Container>
     <h1>* Blogs</h1>
-    <p>Coming soon...</p>
+    <a
+      className="article"
+      href="https://www.hov.co/blog/post/ill-tell-you-6-specific-things-to-stop-doing-and-win-at-life-could-be-offensive"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      How to win at live. Could be offensive.
+    </a>
+    <p>More to come... tech & non-tech</p>
   </Container>
 )
 
